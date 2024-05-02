ea36c616480132c8a22c4cb2d2c76a905583d0f0
tateviky <tatev.yayloyan@gmail.com>
2024-05-02 13:24:31 +0400

    added 'enable' and 'delete' cases for genres

--- cypress/e2e/d_genres.cy.ts
+++ cypress/e2e/d_genres.cy.ts
@@ -8,7 +8,7 @@ beforeEach(() =>{
      cy.task("connectDB", 'delete from genres')
      login.login()
      genres.open_genres_page()
-});
+})
 
 it('Create new genres', () =>{
      let genresName = Date().toLocaleUpperCase()
@@ -32,7 +32,8 @@ it('Create new genres and edit it', () => {
      
      cy.reload()
 
-     cy.get('table').contains('td','NewGenresToEdit').get('[class="p-element p-button-rounded p-button-success mr-2 p-button p-component p-button-icon-only"]').click()
+     cy.get('table').contains('td','NewGenresToEdit').get('[class="p-element p-button-rounded p-button-success mr-2 p-button p-component p-button-icon-only"]')
+                                                       .click()
      cy.get('[class="p-overflow-hidden"]').get('[id="inputtext"]').clear().type('EditedGenres')
      cy.get('[class="p-button-label"]').contains('Save').click()
      
@@ -40,8 +41,37 @@ it('Create new genres and edit it', () => {
 
      cy.get('table').contains('td','NewGenresToEdit').should('not.exist')
      cy.get('table').contains('td','EditedGenres').should('exist')
-}
-)
+})
+
+it('Create new and enable', () => {
+     genres.create_new_genres()
+     cy.get('[id="inputtext"]').clear().type('NewGenresToEdit')
+     cy.get('[class="p-button-label"]').contains('Save').click()
+     
+     cy.reload()
+
+     cy.get('table').contains('td','ENABLED').should('be.visible')
+    
+     cy.get('table').contains('td','NewGenresToEdit').get('[class="p-element p-button-rounded p-button-info mr-2 p-button p-component p-button-icon-only"]')
+                              .click()
+     cy.get('table').contains('td','ENABLED').should('not.exist')
+     cy.get('table').contains('td','DISABLED').should('be.visible')
+})
+
+it.only('Create new genres and delete it', ()=>{
+     genres.create_new_genres()
+     cy.get('[id="inputtext"]').clear().type('NewGenresToEdit')
+     cy.get('[class="p-button-label"]').contains('Save').click()
+     
+     cy.reload()
+     cy.get('table').contains('td','NewGenresToEdit').should('exist')
+     cy.get('table').contains('td','NewGenresToEdit').get('[class="p-element p-button-rounded p-button-warning p-button p-component p-button-icon-only"]')
+                              .click()
+     cy.get('[class="p-button-label"]').contains('Yes').click()
+     cy.reload()
+     cy.get('table').contains('td','NewGenresToEdit').should('not.exist')
+})
+
 
 /*it('sort by id',() => {
    cy.get('[psortablecolumn="id"][class="p-element p-sortable-column"]')
